be17f87d146fa471fa2d8a5a39833dba0c9bdadf
atorres98 <37823055+atorres98@users.noreply.github.com>
2018-09-07 11:43:41 -0400

    fixed buggy code

--- Triangle.py
+++ Triangle.py
@@ -31,27 +31,27 @@ def classifyTriangle(a,b,c):
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
 
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0: """originally had b <= b"""
         return 'InvalidInput'
 
     # verify that all 3 inputs are integers
     # Python's "isinstance(object,type) returns True if the object is of the specified type
     if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
+        return 'InvalidInput' """unnecessary semicolon removed"""
 
     # This information was not in the requirements spec but
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)): """had minus sign instead of plus sign for 2 arguments"""
         return 'NotATriangle'
 
     # now we know that we have a valid triangle
-    if a == b and b == a:
+    if a == b == c: """originally had a==b and b==a"""
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif ((a ** 2) + (b ** 2)) == (c ** 2): """only had one asterisk when it was supposed to be squared"""
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
+    elif (a != b) and  (b != c) and (a != c): """had (a !=b) twice"""
         return 'Scalene'
     else:
         return 'Isoceles'
