85467da3ee4b4091c627cbd714f6b5b33b390b7a
atorres98 <37823055+atorres98@users.noreply.github.com>
2018-09-07 19:23:23 -0400

    edits

--- TestTriangle.py
+++ TestTriangle.py
@@ -28,8 +28,8 @@ class TestTriangles(unittest.TestCase):
 
 """ My additional test cases below """
     def testRightTriangles(self): """test only right triangles"""
-        self.assertEqual(classifyTriangle(3,4,5), 'Right', '3,4,5 is a Right triangle')
-        self.assertEqual(classifyTriangle(5,12,13), 'Right', '5, 12, 13 is a Right triangle')
+        self.assertEqual(classifyTriangle(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(5, 12, 13), 'Right', '5, 12, 13 is a Right triangle')
         self.assertEqual(classifyTriangles(8, 15, 17), 'Right', '5, 12, 13 is a Right triangle')
 
     def testIsoscelesTriangles(self): """test only isosceles triangles"""
@@ -45,7 +45,7 @@ class TestTriangles(unittest.TestCase):
     def testScaleneTraingles(self): """test scalene triangles only""""
         self.assertEqual(classifyTriangle(2, 3, 4), 'Scalene', '2, 3, 4 is a Scalene triangle')
         self.assertEqual(classifyTriangle(3, 6, 11), 'Scalene', '3, 6, 11 is a Scalene triangle')
-        self.assertEqual(classifyTriangle(8, 1, 5), 'Scalene', '8, 5, 7 is a Scalene triangle')
+        self.assertEqual(classifyTriangle(8, 7, 5), 'Scalene', '8, 7, 5 is a Scalene triangle')
 
     def testOrderOfTriangles(self): """test to see if the order of the sides matters for right and isosceles triangles"""
         self.assertEqual(classifyTriangle(12, 13, 5), 'Right', '12, 13, 5 is a Right triangle')
